0a722e4bc2dd6e1a7b42e8c8f4df0a93caf61d26
Dimash <dimash.junusov@gmail.com>
2016-09-18 20:28:44 +0600

    New

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -5,8 +5,16 @@
 angular.module('LunchCheck', [])
 
 .controller('LunchCheckController', function($scope){
-	$scope.asdf = "";
-		
+	$scope.menu = "";
+	$scope.check = "";
+	$scope.message = function() {
+		$scope.menuItems = $scope.menu.split(',');
+		if( $scope.menuItems.length-1 == 0 )
+			return "Please enter data first";
+		else if($scope.menuItems.length-1 < 3)
+			return "Enjoy!";
+		else return "Too much!";
+	};
 });
 
 })();
